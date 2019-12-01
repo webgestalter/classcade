4d5b45b166b68b1c81dae2a0ff5341e09233168f
webgestalter <git@webgestalter.in>
2019-12-01 11:16:41 +0100

    ...

--- classcade.js
+++ classcade.js
@@ -178,11 +178,13 @@ var qsa=s=>(document.querySelectorAll(s)),
 
           ////////// SET VALUE
 
-          var v = s[1];
+          var p = s[0],
+              v = s[1];
           v = values[v] || v;
-          if( s[0] === 'bg' && ( v.length === 6 || v.length === 8 )     ){ console.log(v+' is a hex-color');      v = '#'+v             }
-          if( v.endsWith === 'p' && isNaN(v.charAt(v.length-2))         ){ console.log(v+' is a Prozentangabe');  v = v.slice(0,-1)+'%' }
-          if( ( s[0] === 'h' || s[0] === 'w' ) && isNaN(v.slice(-1))    ){ console.log(v+' is a Teilungsangabe'); v = 'calc(100%/${v})' }
+
+          if( p === 'bg' && ( v.length === 6 || v.length === 8 ) ){ console.log(v+' is a hex-color');      v = '#'+v             }
+          if( v.endsWith === 'p' && isNaN(v.charAt(v.length-2))  ){ console.log(v+' is a Prozentangabe');  v = v.slice(0,-1)+'%' }
+          if( isNaN(v.slice(-1)) && ( p === 'h' || p === 'w' )   ){ console.log(v+' is a Teilungsangabe'); v = 'calc(100%/${v})' }
 
           var c = b || a ;
           qsa('.'+c).forEach(z=>{z.style[p]=v}); //
