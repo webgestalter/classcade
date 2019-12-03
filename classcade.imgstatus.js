d84ac6b8d6b8a22633ec6890b5f3f9e282283612
webgestalter <git@webgestalter.in>
2019-12-03 15:49:21 +0100

    ...

--- classcade.imgstatus.js
+++ classcade.imgstatus.js
@@ -19,36 +19,37 @@
             return console.log('[imgStatus]: There aren\'t any images associated with this selector (' + selector + ')!');
 
         this.total = images.length;
-        for (var i = 0; i < this.total; i++) {
-            if (isCached(images[i].src))
+        for( var i = 0; i < this.total; i++ ){
+            if( isCached(images[i].src) ){
                 this._setLoaded(fn);
-            else if (images[i].addEventListener) {
-                images[i].addEventListener('load', this._setLoaded.bind(this, fn));
-                images[i].addEventListener('error', this._setFailed.bind(this, fn));
-            }
-            else {
-                images[i].attachEvent('onload', this._setLoaded.bind(this, fn));
-                images[i].attachEvent('onerror', this._setFailed.bind(this, fn));
+            } else if ( images[i].addEventListener ){
+                images[i].addEventListener('load', this._setLoaded.bind(this,fn));
+                images[i].addEventListener('error',this._setFailed.bind(this,fn));
+            } else {
+                images[i].attachEvent('onload', this._setLoaded.bind(this,fn));
+                images[i].attachEvent('onerror',this._setFailed.bind(this fn));
             }
         }
     }
 
-    this.isCached = function(src) {
+    this.isCached = function(src){
         var image = new Image();
         image.src = src;
         return image.complete;
     }
 
-    this._setFailed = function(fn, e) {
-        ++this.failed;
-        if (typeof fn === "function")
-            fn(this);
+    this._setFailed = function(fn,e){
+      ++this.failed;
+      if( typeof fn === 'function' ){
+        fn(this);
+      }
     }
 
-    this._setLoaded = function(fn, e) {
-        ++this.loaded;
-        if (typeof fn === "function")
-            fn(this);
+    this._setLoaded = function(fn,e){
+      ++this.loaded;
+      if( typeof fn === 'function' ){
+        fn(this);
+      }
     };
 
     this.isDone = function() {
