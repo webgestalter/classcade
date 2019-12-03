d45051a44fbd170d74131f9f9ac16b70d2f4242e
webgestalter <git@webgestalter.in>
2019-12-03 16:15:40 +0100

    ...

--- classcade.imgstatus.js
+++ classcade.imgstatus.js
@@ -1,57 +1,29 @@
-/*!
-   --------------------------------
-   imgStatus.js
-   --------------------------------
-   + https://github.com/raphamorim/imgstatus
-   + version 0.1.2
-   + Copyright 2015 Raphael Amorim
-   + Licensed under the MIT license
-   + Documentation: https://github.com/raphamorim/imgstatus
-*/
-
-;(function() {
+;( () => {
     this.loaded     = 0;
     this.failed     = 0;
     this.total      = 0;
-    this.watch      = function(selector,fn){
-        var images = document.querySelectorAll(selector);
-        if (!images.length)
-            return console.log('[imgStatus]: There aren\'t any images associated with this selector (' + selector + ')!');
+    this.watch      = (selector,fn) => {
+        var el = document.querySelectorAll(selector);
+        if (!el.length){ return console.log('[imgStatus]: There aren\'t any images associated with this selector (' + selector + ')!') }
 
-        this.total = images.length;
+        this.total = el.length;
         for( var i = 0; i < this.total; i++ ){
-            if( isCached(images[i].src) ){
+            if( isCached(el[i].src) ){
                 this._setLoaded(fn);
-            } else if ( images[i].addEventListener ){
-                images[i].addEventListener('load', this._setLoaded.bind(this,fn));
-                images[i].addEventListener('error',this._setFailed.bind(this,fn));
+            } else if ( el[i].addEventListener ){
+                el[i].addEventListener('load', this._setLoaded.bind(this,fn));
+                el[i].addEventListener('error',this._setFailed.bind(this,fn));
             } else {
-                images[i].attachEvent('onload', this._setLoaded.bind(this,fn));
-                images[i].attachEvent('onerror',this._setFailed.bind(this,fn));
+                el[i].attachEvent('onload', this._setLoaded.bind(this,fn));
+                el[i].attachEvent('onerror',this._setFailed.bind(this,fn));
             }
         }
     }
-    this.isCached   = src => {
-        var img = new Image();
-        img.src = src;
-        return img.complete;
-    }
-    this._setFailed = (fn,e) => {
-      ++this.failed;
-      if( typeof fn === 'function' ){
-        fn(this);
-      }
-    }
-    this._setLoaded = (fn,e) => {
-      ++this.loaded;
-      if( typeof fn === 'function' ){
-        fn(this);
-      }
-    };
-    this.isDone     = () => {
-        return ((this.loaded + this.failed) === this.total)? true:false;
-    }
+    this.isCached   = src    => { var img = new Image(); img.src = src; return img.complete };
+    this._setFailed = (fn,e) => { ++this.failed; if( typeof fn === 'function' ){ fn(this) } };
+    this._setLoaded = (fn,e) => { ++this.loaded; if( typeof fn === 'function' ){ fn(this) } };
+    this.isDone     = ()     => ( ( (this.loaded + this.failed) === this.total) ? true : false )
 
-    if (typeof window === "object")
-        window.imgStatus = this;
+    if( typeof window === "object" ){ window.imgStatus = this }
+
 }());
