616cca6798e6fd5d9989013d2c9c5b639648317d
webgestalter <git@webgestalter.in>
2019-12-03 23:14:28 +0100

    ....

--- classcade.js
+++ classcade.js
@@ -188,7 +188,7 @@ var qsa=s=>(document.querySelectorAll(s)),
           v = s[1];
           v = values[v] || v;
 
-          if( w === 'bg' && ( v.length === 6 || v.length === 8 )  ){ v = '#'+v             };
+          if( w === 'bg' && ( v.length === 6 || v.length === 8 )  ){ console.log('COLOR! '+v); v = '#'+v             };
           if( v.endsWith('p') && !isNaN(v.charAt(v.length-2))     ){ v = v.slice(0,-1)+'%' };
           if( !isNaN(v.slice(-1)) && ( w === 'h' || w === 'w' )   ){ v = 'calc(100%/${v})' };
 
