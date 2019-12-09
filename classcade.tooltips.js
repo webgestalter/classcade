8aed1014a827e9329231c5f46e54026ea214034d
webgestalter <git@webgestalter.in>
2019-12-09 03:29:19 +0100

    ...

--- classcade.tooltips.js
+++ classcade.tooltips.js
@@ -1,134 +1,106 @@
-function tlite(getTooltipOpts) {
-  document.addEventListener('mouseover', function (e) {
-    var el = e.target;
-    var opts = getTooltipOpts(el);
-
-    if (!opts) {
-      el = el.parentElement;
-      opts = el && getTooltipOpts(el);
-    }
-
-    opts && tlite.show(el, opts, true);
+var  cctooltip = getTooltipOpts   => {
+  D.addEventListener(MO,e => {
+    var el = e.target, opts = getTooltipOpts(el);
+    if(!opts){ el = el.parentElement; opts = el && getTooltipOpts(el) }
+    opts && cctooltip.show(el,opts,T)
   });
-}
-
-tlite.show = function (el, opts, isAuto) {
-  var fallbackAttrib = 'data-tlite';
+};
+cctooltip.show = (el,opts,isAuto) => {
+  var fallbackAttrib = 'data-cctooltip';
   opts = opts || {};
 
-  (el.tooltip || Tooltip(el, opts)).show();
+  (el.tooltip || Tooltip(el,opts)).show();
 
-  function Tooltip(el, opts) {
-    var tooltipEl;
-    var showTimer;
-    var text;
+  var Tooltip = (el,opts) => {
+    var ttEl, showTimer, text;
 
-    el.addEventListener('mousedown', autoHide);
-    el.addEventListener('mouseleave', autoHide);
+    el.addEventListener(MD,autoHide); el.addEventListener(ML,autoHide);
 
-    function show() {
-      text = el.title || el.getAttribute(fallbackAttrib) || text;
-      el.title = '';
-      el.setAttribute(fallbackAttrib, '');
-      text && !showTimer && (showTimer = setTimeout(fadeIn, isAuto ? 150 : 1))
-    }
+    var   show = ()           => { text = el.title || el.getAttribute(fallbackAttrib) || text; el.title = ''; el.setAttribute(fallbackAttrib,''); text && !showTimer && (showTimer = setTimeout(fadeIn, isAuto ? 150 : 1)) },
+      autoHide = ()           => { cctooltip.hide(el,T) },
+          hide = isAutoHiding => { if (isAuto === isAutoHiding){ showTimer = clearTimeout(showTimer); var parent = ttEl && ttEl.parentNode; parent && parent.removeChild(ttEl); ttEl = U } },
+        fadeIn = ()           => { if(!ttEl){ ttEl = createTooltip(el,text,opts) } };
 
-    function autoHide() {
-      tlite.hide(el, true);
-    }
+    return el.tooltip = { show: show, hide: hide }
+  },
 
-    function hide(isAutoHiding) {
-      if (isAuto === isAutoHiding) {
-        showTimer = clearTimeout(showTimer);
-        var parent = tooltipEl && tooltipEl.parentNode;
-        parent && parent.removeChild(tooltipEl);
-        tooltipEl = undefined;
-      }
-    }
+  createTooltip = (el,text,opts) => {
+    var ttEl = D.createElement('span'), grav = opts.grav || el.getAttribute('data-cctooltip') || 'n';
+    ttEl.innerHTML = text;
+    el.appendChild(ttEl);
 
-    function fadeIn() {
-      if (!tooltipEl) {
-        tooltipEl = createTooltip(el, text, opts);
-      }
-    }
+    var vertGrav = grav[0] || '',
+        horzGrav = grav[1] || '',
 
-    return el.tooltip = {
-      show: show,
-      hide: hide
-    };
-  }
+    positionTooltip = () => {
+      ttEl.className = 'cctooltip ' + 'cctooltip-' + vertGrav + horzGrav;
 
-  function createTooltip(el, text, opts) {
-    var tooltipEl = document.createElement('span');
-    var grav = opts.grav || el.getAttribute('data-tlite') || 'n';
+      var arrowSize = 10, top = el.offsetTop, left = el.offsetLeft;
 
-    tooltipEl.innerHTML = text;
+      if (ttEl.offsetParent === el){ top = left = 0 }
 
-    el.appendChild(tooltipEl);
+      var    width = el.offsetWidth,
+            height = el.offsetHeight,
+          ttHeight = ttEl.offsetHeight,
+          ttWidth  = ttEl.offsetWidth,
+         centerEl  = left + (width / 2);
 
-    var vertGrav = grav[0] || '';
-    var horzGrav = grav[1] || '';
-
-    function positionTooltip() {
-      tooltipEl.className = 'tlite ' + 'tlite-' + vertGrav + horzGrav;
-
-      var arrowSize = 10;
-      var top = el.offsetTop;
-      var left = el.offsetLeft;
-
-      if (tooltipEl.offsetParent === el) {
-        top = left = 0;
-      }
-
-      var width = el.offsetWidth;
-      var height = el.offsetHeight;
-      var tooltipHeight = tooltipEl.offsetHeight;
-      var tooltipWidth = tooltipEl.offsetWidth;
-      var centerEl = left + (width / 2);
-
-      tooltipEl.style.top = (
-        vertGrav === 's' ? (top - tooltipHeight - arrowSize) :
-        vertGrav === 'n' ? (top + height + arrowSize) :
-        (top + (height / 2) - (tooltipHeight / 2))
+      ttEl.style.top = (
+        vertGrav === 's' ? (top - ttHeight - arrowSize)
+      : vertGrav === 'n' ? (top + height + arrowSize)
+                         : (top + (height / 2) - (ttHeight / 2))
       ) + 'px';
 
-      tooltipEl.style.left = (
-        horzGrav === 'w' ? left :
-        horzGrav === 'e' ? left + width - tooltipWidth :
-        vertGrav === 'w' ? (left + width + arrowSize) :
-        vertGrav === 'e' ? (left - tooltipWidth - arrowSize) :
-        (centerEl - tooltipWidth / 2)
-      ) + 'px';
+      ttEl.style.left = (
+        horzGrav === 'w' ? left
+      : horzGrav === 'e' ? left + width - ttWidth
+      : vertGrav === 'w' ? (left + width + arrowSize)
+      : vertGrav === 'e' ? (left - ttWidth - arrowSize)
+                         : (centerEl - ttWidth / 2)
+      ) + 'px'
     }
 
     positionTooltip();
 
-    var rect = tooltipEl.getBoundingClientRect();
+    var rect = ttEl.getBoundingClientRect();
 
-    if (vertGrav === 's' && rect.top < 0) {
-      vertGrav = 'n';
-      positionTooltip();
-    } else if (vertGrav === 'n' && rect.bottom > window.innerHeight) {
-      vertGrav = 's';
-      positionTooltip();
-    } else if (vertGrav === 'e' && rect.left < 0) {
-      vertGrav = 'w';
-      positionTooltip();
-    } else if (vertGrav === 'w' && rect.right > window.innerWidth) {
-      vertGrav = 'e';
-      positionTooltip();
-    }
+    vertGrav = (vertGrav === 's' && rect.top < 0)                ? 'n'
+             : (vertGrav === 'n' && rect.bottom > W.innerHeight) ? 's'
+             : (vertGrav === 'e' && rect.left < 0)               ? 'w'
+             : (vertGrav === 'w' && rect.right > W.innerWidth)   ? 'e'
+                                                                 : '' ;
+    positionTooltip();
 
-    tooltipEl.className += ' tlite-visible';
-
-    return tooltipEl;
+    ttEl.className += ' cctooltip-visible';
+    return ttEl;
   }
 };
+cctooltip.hide = (el,isAuto)      => { el.tooltip && el.tooltip.hide(isAuto) };
 
-tlite.hide = function (el, isAuto) {
-  el.tooltip && el.tooltip.hide(isAuto);
-};
+if( typeof module !== 'undefined' && module.exports ){ module.exports = cctooltip }
 
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = tlite;
-}
+
+// a = arrowSize
+// _ =
+// c = centerEl
+// f = fallbackAttrib
+// g = horzGrav
+// h = height
+// i = isAutoHiding
+// l = left
+// o = opts
+// p = parent
+// r = rect
+// s = showTimer
+// t = top
+// _ =
+// v = vertGrav
+// w = width
+// z = ttEl
+
+autoHide
+grav
+isAuto
+x text
+ttHeight
+ttWidth
