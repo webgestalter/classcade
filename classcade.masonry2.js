52957d5c7e5f3cabe4b5437bdc387b00577336c9
webgestalter <git@webgestalter.in>
2019-12-17 09:33:41 +0100

    ...

--- classcade.masonry2.js
+++ classcade.masonry2.js
@@ -1,29 +1,33 @@
-var resizeItem  = el => {
+var masonry = () => {
 
-  var grid = D.getElementsByClassName('masonry')[0],
-      rowG = parseInt(W.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
-      rowH = parseInt(W.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  var resizeItem  = el => {
 
-  var rowSpan = Math.ceil((QS('.masonry-content',el).getBoundingClientRect().height+rowG)/(rowH+rowG));
+    var grid = D.getElementsByClassName('masonry')[0],
+        rowG = parseInt(W.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
+        rowH = parseInt(W.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
 
-  el.style.gridRowEnd = 'span '+rowSpan;
-},
-    resizeItems = ()   => {
-      var items = D.getElementsByClassName('masonry-brick');
-      for(var i=0; i < L(items); i++){
-        resizeItem(items[i]);
-      }
-},
-    waitForIMG  = ()   => {
-      var items = D.getElementsByClassName('masonry-brick');
-      for(var i=0; i < L(items); i++){
-        imagesLoaded( items[i], instance => {
-          var el = instance.elements[0];
-          resizeItem(el);
-        } );
-      }
-};
+    var rowSpan = Math.ceil((QS('.masonry-content',el).getBoundingClientRect().height+rowG)/(rowH+rowG));
 
-FE( ['load','resize'], e => { AEL(W,e,resizeItems) });
+    el.style.gridRowEnd = 'span '+rowSpan;
+  },
+      resizeItems = ()   => {
+        var items = D.getElementsByClassName('masonry-brick');
+        for(var i=0; i < L(items); i++){
+          resizeItem(items[i]);
+        }
+  },
+      waitForIMG  = ()   => {
+        var items = D.getElementsByClassName('masonry-brick');
+        for(var i=0; i < L(items); i++){
+          imagesLoaded( items[i], instance => {
+            var el = instance.elements[0];
+            resizeItem(el);
+          } );
+        }
+  };
 
-waitForIMG();
+  FE( ['load','resize'], e => { AEL(W,e,resizeItems) });
+
+  waitForIMG();
+
+}
