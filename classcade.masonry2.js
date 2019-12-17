7579902a1cd3b2763b912fbc7960feada1e21e61
webgestalter <git@webgestalter.in>
2019-12-17 08:59:22 +0100

    ...

--- classcade.masonry2.js
+++ classcade.masonry2.js
@@ -1,4 +1,4 @@
-function resizeMasonryItem(item){
+var resizeMasonryItem     = item => {
 
   var      grid = D.getElementsByClassName('masonry')[0],
          rowGap = parseInt(W.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
@@ -7,28 +7,25 @@ function resizeMasonryItem(item){
   var rowSpan = Math.ceil((item.querySelector('.masonry-content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
 
   item.style.gridRowEnd = 'span '+rowSpan;
-}
+},
+    resizeAllMasonryItems = ()   => {
+      var allItems = D.getElementsByClassName('masonry-brick');
+      for(var i=0;i<allItems.length;i++){
+        resizeMasonryItem(allItems[i]);
+      }
+},
+    waitForImages         = ()   => {
+      var allItems = D.getElementsByClassName('masonry-brick');
+      for(var i=0;i<allItems.length;i++){
+        imagesLoaded( allItems[i], instance => {
+          var item = instance.elements[0];
+          resizeMasonryItem(item);
+        } );
+      }
+};
 
-function resizeAllMasonryItems(){
-  var allItems = D.getElementsByClassName('masonry-brick');
-  for(var i=0;i<allItems.length;i++){
-    resizeMasonryItem(allItems[i]);
-  }
-}
-
-function waitForImages() {
-  var allItems = D.getElementsByClassName('masonry-brick');
-  for(var i=0;i<allItems.length;i++){
-    imagesLoaded( allItems[i], function(instance) {
-      var item = instance.elements[0];
-      resizeMasonryItem(item);
-    } );
-  }
-}
-
-var masonryEvents = ['load','resize'];
-masonryEvents.forEach( function(event){
-  W.addEventListener(event, resizeAllMasonryItems);
-} );
+['load','resize'].forEach( event => {
+  W.addEventListener(event,resizeAllMasonryItems);
+});
 
 waitForImages();
