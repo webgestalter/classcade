5c09ca8a00d835036a71ad01edd07ce9f8d41afe
webgestalter <git@webgestalter.in>
2020-03-30 12:43:01 +0200

    ...

--- classcade.imgstatus.js
+++ classcade.imgstatus.js
@@ -1,31 +1,34 @@
 ;(function(){
-    this.loaded     = 0;
-    this.failed     = 0;
-    this.total      = 0;
-    this.watch      = (s,f) => {
-        var el = document.querySelectorAll(s);
-        if (!el.length){
-          return console.log('[imgStatus]: There aren\'t any images associated with this selector (' + s + ')!')
-        }
+  this.loaded = 0;
+  this.failed = 0;
+  this.total  = 0;
+  this.watch  = (s,f) => {
 
-        this.total = el.length;
-        for( var i = 0; i < this.total; i++ ){
-            if( isCached(el[i].src) ){
-                this._setLoaded(f);
-            } else if ( el[i].addEventListener ){
-                el[i].addEventListener('load', this._setLoaded.bind(this,f));
-                el[i].addEventListener('error',this._setFailed.bind(this,f));
-            } else {
-                el[i].attachEvent('onload', this._setLoaded.bind(this,f));
-                el[i].attachEvent('onerror',this._setFailed.bind(this,f));
-            }
-        }
+    var el = document.querySelectorAll(s);
+
+    if (!el.length){
+      return console.log('[imgStatus]: There aren\'t any images associated with this selector (' + s + ')!')
     }
-    this.isCached   = src   => { var img = new Image(); img.src = src; return img.complete };
-    this._setFailed = (f,e) => { ++this.failed; if( typeof f === 'function' ){ f(this) } };
-    this._setLoaded = (f,e) => { ++this.loaded; if( typeof f === 'function' ){ f(this) } };
-    this.isDone     = ()    => ( ( (this.loaded + this.failed) === this.total) ? true : false );
 
-    if( typeof window === "object" ){ window.imgStatus = this }
+    this.total = el.length;
+    for( var i = 0; i < this.total; i++ ){
+      if( isCached(el[i].src) ){
+        this._setLoaded(f);
+      } else if ( el[i].addEventListener ){
+        el[i].addEventListener('load', this._setLoaded.bind(this,f));
+        el[i].addEventListener('error',this._setFailed.bind(this,f));
+      } else {
+        el[i].attachEvent('onload', this._setLoaded.bind(this,f));
+        el[i].attachEvent('onerror',this._setFailed.bind(this,f));
+      }
+    }
+
+  }
+  this.isCached   = src   => { var img = new Image(); img.src = src; return img.complete };
+  this._setFailed = (f,e) => { ++this.failed; if( typeof f === 'function' ){ f(this) } };
+  this._setLoaded = (f,e) => { ++this.loaded; if( typeof f === 'function' ){ f(this) } };
+  this.isDone     = ()    => ( ( (this.loaded + this.failed) === this.total) ? true : false );
+
+  if( typeof window === "object" ){ window.imgStatus = this }
 
 }());
--- classcade.masonry2.js
+++ classcade.masonry2.js
@@ -1,39 +1,28 @@
   var masonryResizeItem  = el => {
 
-    var grid = D.getElementsByClassName('masonry')[0],
-    //  rowG = _(grid).style('grid-row-gap'),
-    //  rowH = _(grid).style('grid-auto-rows'),
-        rowG = parseInt(W.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
-        rowH = parseInt(W.getComputedStyle(grid).getPropertyValue('grid-auto-rows')),
-     rowSpan = Math.ceil((QS('.masonry-content',el).getBoundingClientRect().height+rowG)/(rowH+rowG));
+    var grid = document.getElementsByClassName('masonry')[0],
+        rowG = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
+        rowH = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')),
+     rowSpan = Math.ceil((el.querySelector('.masonry-content').getBoundingClientRect().height+rowG)/(rowH+rowG));
 
     el.style.gridRowEnd = 'span '+rowSpan;
   },
-      masonryResize = ()   => {
-        var items = D.getElementsByClassName('masonry-brick');
-        for(var i=0; i < L(items); i++){
-          masonryResizeItem(items[i]);
-        }
+  masonryResize = () => {
+    var items = document.getElementsByClassName('masonry-brick');
+    for(var i=0; i < L(items); i++){
+      masonryResizeItem(items[i]);
+    }
   },
-      waitForIMG  = ()   => {
-        var items = D.getElementsByClassName('masonry-brick');
-        for(var i=0; i < L(items); i++){
-          imagesLoaded( items[i], instance => {
-            var el = instance.elements[0];
-            masonryResizeItem(el);
-          } );
-        }
-
-        // LOG('waitForIMG() triggered');
-        // imgStatus.watch(".masonry img",function(instance){
-        //   if(instance.isDone()){
-        //     console.log("image loaded");
-        //     var el = _(instance)._('.masonry-brick');
-        //     masonryResizeItem(el);
-        //   }
-        // });
+  waitForIMG = () => {
+    var items = D.getElementsByClassName('masonry-brick');
+    for(var i=0; i < L(items); i++){
+      imagesLoaded( items[i], instance => {
+        var el = instance.elements[0];
+        masonryResizeItem(el);
+      } );
+    }
   };
 
-  FE( ['load','resize'], e => { AEL(W,e,masonryResize) });
+  ['load','resize'].forEach( e => { window.addEventListener(e,masonryResize) });
 
   waitForIMG();
